1b54b4d594e1a6ce5e95e187cc5632dc1ebe7219
brtkrclr <berat.karacalar@optiim.com>
2020-08-07 13:59:04 +0300

    Enter

---
+++ src/api/unsplash.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+
+export default axios.create({
+    baseURL:'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID iHaI_AQHy2iJ-712bH8FBt5jAp0CrYDl95_nDzNb7Ik',
+    }
+
+
+
+});
